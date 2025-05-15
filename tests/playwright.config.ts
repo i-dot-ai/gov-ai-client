2ccd66de2b1d71c6c83d96e2a393d2db12661075
Kevin Etchells <kevetchells@hotmail.com>
2025-05-15 15:43:49 +0100

    Add tests for copy-button

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -36,7 +36,10 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { 
+        ...devices['Desktop Chrome'],
+        permissions: ['clipboard-read'],
+      },
     },
 
     {
@@ -46,7 +49,10 @@ export default defineConfig({
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: { 
+        ...devices['Desktop Safari'],
+        permissions: ['clipboard-read'],
+      },
     },
 
     /* Test against mobile viewports. */
