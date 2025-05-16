0c9f0121916f79441d91e8d9357e475dcc3b6d75
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-05-16 11:17:11 +0100

    Stepped back certain IP-related versions

--- terraform/load_balancer.tf
+++ terraform/load_balancer.tf
@@ -1,7 +1,7 @@
 module "load_balancer" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
   #source           = "../../i-dot-ai-core-terraform-modules//modules/infrastructure/load_balancer" # For testing local changes
-  source            = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/load_balancer?ref=v2.0.1-load_balancer"
+  source            = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/load_balancer?ref=v1.2.1-load_balancer"
   name              = local.name
   account_id        = data.aws_caller_identity.current.account_id
   vpc_id            = data.terraform_remote_state.vpc.outputs.vpc_id
@@ -9,17 +9,16 @@ module "load_balancer" {
   ip_whitelist      = concat(var.internal_ips, var.developer_ips)
   certificate_arn   = data.terraform_remote_state.universal.outputs.certificate_arn
   web_acl_arn       = module.waf.web_acl_arn
-  env               = var.env 
   
 }
 
 module "waf" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
   #source        = "../../i-dot-ai-core-terraform-modules//modules/infrastructure/waf" # For testing local changes
-  source         = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/waf?ref=v6.0.0-waf"
+  source         = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/waf?ref=v5.2.0-waf"
   name           = local.name
   host           = local.host
-  env            = var.env
+  ip_set         = concat(var.internal_ips, var.developer_ips)
 }
 
 resource "aws_route53_record" "type_a_record" {
