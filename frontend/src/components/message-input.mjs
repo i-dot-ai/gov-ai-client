3c8e13612b7529b7d66a09c8d9ed3254ab28f014
Kevin Etchells <kevetchells@hotmail.com>
2025-06-19 10:11:24 +0100

    Add accessibility improvements to tools-autocomplete

--- frontend/src/components/message-input.mjs
+++ frontend/src/components/message-input.mjs
@@ -16,25 +16,6 @@ export class MessageInput extends HTMLElement {
       return;
     }
 
-    // only show prompt box if '@' used
-    /** @type {HTMLElement | null} */
-    /*
-    const suggestions = document.querySelector('#prompt__listbox');
-    if (suggestions) {
-      suggestions.style.display = 'none';
-      this.textarea.addEventListener('keyup', () => {
-        if (!this.textarea) {
-          return;
-        }
-        const showSuggestions = this.textarea.value.charAt(0) === '@'
-        suggestions.style.display = showSuggestions ? 'block' : 'none';
-        if (!showSuggestions) {
-          this.textarea.setAttribute('aria-expanded', 'false');
-        }
-      });
-    }
-    */
-
     // Submit form on enter-key press (providing shift isn't being pressed)
     this.textarea.addEventListener('keypress', (evt) => {
       if (evt.key === 'Enter' && !evt.shiftKey) {
@@ -83,9 +64,92 @@ export class MessageInput extends HTMLElement {
 
 
   #setupAutocomplete() {
+
     if (!this.textarea) {
       return;
     }
+
+    this.textarea.setAttribute('aria-haspopup', 'listbox');
+    this.textarea.setAttribute('aria-controls', 'tools-list');
+    this.textarea.setAttribute('aria-expanded', 'false');
+
+    // create live-region for screen-reader announcements
+    let announcement = document.createElement('div');
+    announcement.id = 'tool-autocomplete-live-region';
+    announcement.classList.add('govuk-visually-hidden');
+    announcement.setAttribute('aria-live', 'assertive');
+    announcement.setAttribute('role', 'status');
+    this.appendChild(announcement);
+
+    /**
+     * One time setup for the autocomplete list
+     * @param { HTMLUListElement } menu 
+     */
+    const setupList = (menu) => {
+      
+      menu.setAttribute('role', 'listbox');
+      menu.setAttribute('id', 'tools-list');
+      
+      const observerList = new MutationObserver(() => {
+        menu.querySelectorAll('li').forEach((item) => {
+          item.setAttribute('role', 'option');
+          item.setAttribute('id', `tools-option-${item.dataset.index}`);
+        });
+      });
+      observerList.observe(menu, { childList: true, subtree: true});
+
+      const observerListItem = new MutationObserver(() => {
+        const highlighted = menu.querySelector('.highlight');
+        if (highlighted) {
+          this.textarea?.setAttribute('aria-activedescendant', highlighted.id);
+        }
+      });
+      observerListItem.observe(menu, { subtree: true, attributes: true, attributeFilter: ['class'] });
+      
+    };
+
+    // when the autocomplete list shows
+    this.textarea.addEventListener('tribute-active-true', () => {
+
+      /** @type { HTMLUListElement | null } */
+      let menu = document.querySelector('.tribute-container ul');
+      if (menu && !menu.id) {
+        setupList(menu);
+      }
+
+      this.textarea?.setAttribute('aria-expanded', 'true');
+      window.setTimeout(() => {
+        announcement.textContent = `Expanded'. ${menu?.querySelector('.highlight')?.textContent}, menu item`;   
+      }, 100);
+
+    });
+
+    // when the autocomplete list closes
+    this.textarea.addEventListener('tribute-active-false', () => {
+      this.textarea?.setAttribute('aria-expanded', 'false');
+      this.textarea?.removeAttribute('aria-activedescendant');
+      /*
+      The only way to get this to announce is to remove focus from textarea until announcement has finished
+      window.setTimeout(() => {
+        announcement.textContent = 'Collapsed';
+      }, 100);
+      */
+    });
+
+    /*
+    When an item is selected
+    The only way to get this to announce is to remove focus from textarea until announcement has finished
+    this.textarea.addEventListener('tribute-replaced', (evt) => {
+      const insertedItem = evt.detail.item.original.value;
+      this.textarea?.blur();
+      announcement.textContent = 'Added';
+      window.setTimeout(() => {
+        this.textarea?.focus();
+      }, 500);
+    });
+    */
+
+    // setup tribute
     this.tribute = new Tribute({
       values: (searchText, cb) => {
         const allTools = [...document.querySelectorAll('input[name="servers"]:checked ~ div .js-tool')].map(element => {
@@ -98,6 +162,7 @@ export class MessageInput extends HTMLElement {
       }
     });
     this.tribute.attach(this.textarea);
+
   }
 
 }
