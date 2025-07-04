ac093345f8c54b925babe12296772fac90ec7b83
Kevin Etchells <kevetchells@hotmail.com>
2025-07-04 15:51:24 +0100

    Add ESLint and apply fixes

--- frontend/src/components/tool-selector.ts
+++ frontend/src/components/tool-selector.ts
@@ -5,7 +5,7 @@
  * Stores MCP Server selection in localStorage
  */
 const ToolSelector = class extends HTMLElement {
-  
+
   connectedCallback() {
 
     const localStorageStr = window.localStorage.getItem('selected-plugins');
@@ -16,17 +16,17 @@ const ToolSelector = class extends HTMLElement {
     inputs.forEach((input) => {
 
       input.checked = !localStorageStr || preselectedPlugins.includes(input.value);
-      
+
       input.addEventListener('click', () => {
         const selectedInputs = [...inputs]
           .filter((input) => input.checked)
           .map((input) => input.value);
-        window.localStorage.setItem(`selected-plugins`, JSON.stringify(selectedInputs));
+        window.localStorage.setItem('selected-plugins', JSON.stringify(selectedInputs));
       });
 
     });
   }
 
-}
+};
 
-customElements.define("tool-selector", ToolSelector);
+customElements.define('tool-selector', ToolSelector);
