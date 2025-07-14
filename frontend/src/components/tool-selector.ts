2f9261af8390ea457a15293a4d494414f770b373
Kevin Etchells <kevetchells@hotmail.com>
2025-07-14 08:27:35 +0100

    Improve behaviour of expanding tool-selector
    linting
    
    linting

--- frontend/src/components/tool-selector.ts
+++ frontend/src/components/tool-selector.ts
@@ -8,23 +8,28 @@ const ToolSelector = class extends HTMLElement {
 
   connectedCallback() {
 
+    // Remember selected plugins and tools
     const localStorageStr = window.localStorage.getItem('selected-tools');
     const preselectedTools = JSON.parse(localStorageStr || '[]');
-
     const inputs: NodeListOf<HTMLInputElement> = this.querySelectorAll('input[type="checkbox"]');
-
     inputs.forEach((input) => {
-
       input.checked = !localStorageStr || preselectedTools.includes(input.value);
-
       input.addEventListener('click', () => {
         const selectedInputs = [...inputs]
           .filter((input) => input.checked)
           .map((input) => input.value);
         window.localStorage.setItem('selected-tools', JSON.stringify(selectedInputs));
       });
-
     });
+
+    // Scroll down when "Select plugins" expanded
+    const details = document.querySelector('details');
+    details?.addEventListener('toggle', () => {
+      if (details.open) {
+        window.scrollTo(0, document.body.scrollHeight);
+      }
+    });
+
   }
 
 };
