5c4c3303b1fe230cfabd22f6a05b1565eb4f23c1
Kevin Etchells <kevetchells@hotmail.com>
2025-07-14 08:09:22 +0100

    Ensure all tools are selected by default

--- frontend/src/components/tool-selector.ts
+++ frontend/src/components/tool-selector.ts
@@ -8,20 +8,20 @@ const ToolSelector = class extends HTMLElement {
 
   connectedCallback() {
 
-    const localStorageStr = window.localStorage.getItem('selected-plugins');
-    const preselectedPlugins = JSON.parse(localStorageStr || '[]');
+    const localStorageStr = window.localStorage.getItem('selected-tools');
+    const preselectedTools = JSON.parse(localStorageStr || '[]');
 
     const inputs: NodeListOf<HTMLInputElement> = this.querySelectorAll('input[type="checkbox"]');
 
     inputs.forEach((input) => {
 
-      input.checked = !localStorageStr || preselectedPlugins.includes(input.value);
+      input.checked = !localStorageStr || preselectedTools.includes(input.value);
 
       input.addEventListener('click', () => {
         const selectedInputs = [...inputs]
           .filter((input) => input.checked)
           .map((input) => input.value);
-        window.localStorage.setItem('selected-plugins', JSON.stringify(selectedInputs));
+        window.localStorage.setItem('selected-tools', JSON.stringify(selectedInputs));
       });
 
     });
