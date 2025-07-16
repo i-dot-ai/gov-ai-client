2ef3cdcb8b501b0361a12c123900bd8e4098b0c7
Danny Burke <danny.burke@cabinetoffice.gov.uk>
2025-07-16 10:08:24 +0100

    use ChatOpenAI library instead of Azure and provide specific configuration

--- frontend/src/logic/ai3.ts
+++ frontend/src/logic/ai3.ts
@@ -6,7 +6,7 @@
 import 'dotenv/config';
 
 import { createReactAgent } from '@langchain/langgraph/prebuilt';
-import { AzureChatOpenAI } from '@langchain/openai';
+import { ChatOpenAI } from '@langchain/openai';
 import { AIMessage, HumanMessage, SystemMessage } from '@langchain/core/messages';
 import { sendMessage } from '../pages/api/sse';
 import { mcpServers } from './get-servers.ts';
@@ -33,13 +33,14 @@ export type Message = {
 };
 
 
-export const getLlmResponse = async(messages: Message[], selectedServers: string[], selectedTools: string[], authToken: string) => {
+export const getLlmResponse = async (messages: Message[], selectedServers: string[], selectedTools: string[], authToken: string) => {
 
-  const agentModel = new AzureChatOpenAI({
+  const agentModel = new ChatOpenAI({
     openAIApiKey: process.env['LITELLM_GOVAI_CLIENT_OPENAI_API_KEY'],
-    openAIApiVersion: process.env['OPENAI_API_VERSION'],
-    openAIBasePath: process.env['LLM_GATEWAY_URL'],
-    deploymentName: 'o4-mini',
+    configuration: {
+      baseURL: process.env['LLM_GATEWAY_URL'],
+    },
+    modelName: 'azure/o4-mini',
     callbackManager,
   });
 
