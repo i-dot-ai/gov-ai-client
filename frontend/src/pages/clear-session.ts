c1a2b6620594f7b0c8215fcb8de147321d3647ad
Kevin Etchells <kevetchells@hotmail.com>
2025-07-30 12:43:23 +0100

    Make clearing sessions more robust

--- frontend/src/pages/clear-session.ts
+++
@@ -1,8 +0,0 @@
-import type { APIContext } from 'astro';
-
-export async function GET(context: APIContext) {
-  const scope = context.url.searchParams.get('scope') || 'all';
-  console.log('Clearing messages for:', `messages-${scope}`);
-  context.session?.set(`messages-${scope}`, [], {ttl: 1});
-  return context.redirect( `/${scope}`);
-}
